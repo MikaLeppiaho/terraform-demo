49e08ae3763a59da6a37aedd733f829ad6ed1b36
MikaLeppiaho <mika.leppiaho@sulava.com>
2025-06-08 22:39:22 +0300

    updated to use virtual machines

--- main.tf
+++ main.tf
@@ -4,47 +4,151 @@ resource "azurerm_resource_group" "rg" {
   location = each.value
 }
 
-# Add Azure Container Registry
-resource "azurerm_container_registry" "acr" {
-  name                = "${var.resource_prefix}acr${random_string.acr_suffix.result}"
-  resource_group_name = azurerm_resource_group.rg[var.regions[0]].name
-  location            = azurerm_resource_group.rg[var.regions[0]].location
-  sku                 = "Basic"
-  admin_enabled       = true
+# Create Virtual Network for each region
+resource "azurerm_virtual_network" "vnet" {
+  for_each            = azurerm_resource_group.rg
+  name                = "${var.resource_prefix}-vnet-${each.key}"
+  address_space       = ["10.0.0.0/16"]
+  location            = each.value.location
+  resource_group_name = each.value.name
+}
+
+# Create Subnet for each region
+resource "azurerm_subnet" "internal" {
+  for_each             = azurerm_resource_group.rg
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.rg[each.key].name
+  virtual_network_name = azurerm_virtual_network.vnet[each.key].name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+# Create Public IP for each VM
+resource "azurerm_public_ip" "vm_public_ip" {
+  for_each            = azurerm_resource_group.rg
+  name                = "${var.resource_prefix}-pip-${each.key}"
+  resource_group_name = each.value.name
+  location            = each.value.location
+  allocation_method   = "Static"
+  sku                 = "Standard"
+  domain_name_label   = lower("${var.resource_prefix}-${each.key}-vm")
+}
+
+# Create Network Security Group and rule for HTTP
+resource "azurerm_network_security_group" "vm_nsg" {
+  for_each            = azurerm_resource_group.rg
+  name                = "${var.resource_prefix}-nsg-${each.key}"
+  location            = each.value.location
+  resource_group_name = each.value.name
+
+  security_rule {
+    name                       = "HTTP"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# Create Network Interface
+resource "azurerm_network_interface" "vm_nic" {
+  for_each            = azurerm_resource_group.rg
+  name                = "${var.resource_prefix}-nic-${each.key}"
+  location            = each.value.location
+  resource_group_name = each.value.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.internal[each.key].id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.vm_public_ip[each.key].id
+  }
+}
+
+# Associate Network Security Group to Network Interface
+resource "azurerm_network_interface_security_group_association" "vm_nsg_association" {
+  for_each                  = azurerm_resource_group.rg
+  network_interface_id      = azurerm_network_interface.vm_nic[each.key].id
+  network_security_group_id = azurerm_network_security_group.vm_nsg[each.key].id
+}
+
+# Generate random text for unique storage account name per region
+resource "random_id" "random_id" {
+  for_each = azurerm_resource_group.rg
+  keepers = {
+    resource_group = azurerm_resource_group.rg[each.key].name
+  }
+  byte_length = 8
+}
+
+# Create storage account for boot diagnostics in each region
+resource "azurerm_storage_account" "vm_storage" {
+  for_each                 = azurerm_resource_group.rg
+  name                     = "diag${random_id.random_id[each.key].hex}"
+  location                 = each.value.location
+  resource_group_name      = each.value.name
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+# Create Virtual Machine
+resource "azurerm_linux_virtual_machine" "vm" {
+  for_each            = azurerm_resource_group.rg
+  name                = "${var.resource_prefix}-vm-${each.key}"
+  location            = each.value.location
+  resource_group_name = each.value.name
+  size                = "Standard_B1s"
+  admin_username      = "adminuser"
+
+  # Disable password authentication and use SSH keys only
+  disable_password_authentication = true
+
+  network_interface_ids = [
+    azurerm_network_interface.vm_nic[each.key].id,
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/id_rsa.pub") # You'll need to generate SSH keys
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Premium_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
+  }
+
+  boot_diagnostics {
+    storage_account_uri = azurerm_storage_account.vm_storage[each.key].primary_blob_endpoint
+  }
+
+  # Install nginx using cloud-init
+  custom_data = base64encode(templatefile("cloud-init.yml", {}))
 }
 
 resource "random_string" "acr_suffix" {
   length  = 8
   special = false
   upper   = false
-}
-
-resource "azurerm_container_group" "container" {
-  for_each            = azurerm_resource_group.rg
-  name                = "${var.resource_prefix}-cg-${each.key}"
-  location            = each.value.location
-  resource_group_name = each.value.name
-  os_type             = "Linux"
-
-  container {
-    name   = "demoapp"
-    image  = "nginx:1.25-alpine" # Use specific version and lighter alpine variant
-    cpu    = "0.5"
-    memory = "1.5"
-
-    ports {
-      port     = 80
-      protocol = "TCP"
-    }
-  }
-
-  ip_address_type = "Public"
-  dns_name_label  = lower("${var.resource_prefix}-${each.key}-dns") # Ensure lowercase for DNS
-
-  # Add retry logic
-  timeouts {
-    create = "10m"
-    update = "10m"
-    delete = "10m"
-  }
 }
--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,47 @@
-output "container_fqdn" {
-  description = "FQDN of deployed containers per region"
+output "vm_public_ips" {
+  description = "Public IP addresses of deployed VMs per region"
   value = {
-    for key, cg in azurerm_container_group.container :
-    key => cg.fqdn
+    for key, pip in azurerm_public_ip.vm_public_ip :
+    key => pip.ip_address
+  }
+}
+
+output "vm_fqdns" {
+  description = "FQDN of deployed VMs per region"
+  value = {
+    for key, pip in azurerm_public_ip.vm_public_ip :
+    key => pip.fqdn
+  }
+}
+
+output "vm_ssh_commands" {
+  description = "SSH commands to connect to VMs"
+  value = {
+    for key, pip in azurerm_public_ip.vm_public_ip :
+    key => "ssh adminuser@${pip.ip_address}"
+  }
+}
+
+output "vm_web_urls" {
+  description = "Web URLs to access nginx on VMs"
+  value = {
+    for key, pip in azurerm_public_ip.vm_public_ip :
+    key => "http://${pip.ip_address}"
+  }
+}
+
+output "vm_details" {
+  description = "Complete VM details per region"
+  value = {
+    for key, vm in azurerm_linux_virtual_machine.vm :
+    key => {
+      name       = vm.name
+      location   = vm.location
+      size       = vm.size
+      public_ip  = azurerm_public_ip.vm_public_ip[key].ip_address
+      fqdn       = azurerm_public_ip.vm_public_ip[key].fqdn
+      ssh_command = "ssh adminuser@${azurerm_public_ip.vm_public_ip[key].ip_address}"
+      web_url    = "http://${azurerm_public_ip.vm_public_ip[key].ip_address}"
+    }
   }
 }
