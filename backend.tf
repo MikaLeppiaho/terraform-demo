0f1bfbe85341697be846648bd15e4d4f5e0a0989
MikaLeppiaho <mika.leppiaho@sulava.com>
2025-06-08 20:45:45 +0300

    update workflow ini command

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "tfstate-rg"             # Luo etukäteen
-    storage_account_name = "tfstate"                # Luo etukäteen
+    storage_account_name = "tfstatemika"                # Luo etukäteen
     container_name       = "state"
     key                  = "demo.tfstate"
   }
